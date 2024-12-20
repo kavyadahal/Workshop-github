d4ee486f693f7134b1ec7fac077cf48b6cc95bcb
Aavash232311 <aavash3150@gmail.com>
2024-12-20 16:59:08 +0545

    joke.c

--- learn.c
+++ learn.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+
+int fib(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
 void main()
 {
-
-printf("Hello World");
-
+    int range = 5;
+    for (int i = 1; i <= range; i++) {
+        int s = fib(i);
+        printf("%d ", s);
+    }
 }
