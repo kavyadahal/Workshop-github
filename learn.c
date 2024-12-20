f16695140d67cacc472be9f303f6713ae8ad4ceb
Aavash232311 <aavash3150@gmail.com>
2024-12-20 17:05:35 +0545

    abcd

--- learn.c
+++ learn.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
 
 int fib(int n)
-{
+{  // fibo recurcive code
     if (n <= 1)
     {
         return 1;
     }
     return fib(n - 1) + fib(n - 2);
+}
+
+void triange() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
+
+void upsideTriange() {
+    for (int i = 5; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
+
+void abcd() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
 
 void main()
@@ -16,4 +43,10 @@ void main()
         int s = fib(i);
         printf("%d ", s);
     }
+    printf("\n");
+    triange();
+    printf("\n");
+    upsideTriange();
+    printf("\n");
+    abcd();
 }
